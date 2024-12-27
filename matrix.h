6a01e71c580ba3afabc453ec398b42a192b5b418
sanya anakonda <58643696+streletsky-oi@users.noreply.github.com>
2024-12-27 12:00:27 +0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,81 +1,63 @@
-#include "vector.h"
 #pragma once
-#include "Vector.h"
+#include "vector.h"
+
 
 template <class T>
-class Matrix: public Vector<Vector<T>>
+class Matrix : public Vector<Vector<T>>
 {
-private:
-    int _deg = 1;
 public:
-    Matrix(size_t n): Vector<Vector<T>>(n, 0)
-    {
-        for(size_t i = 0; i<n; i++){
-            this->_array[i] = Vector<T>(n-i,i);
-        } 
-    }
-    
-    Matrix(const Matrix& mt):Vector<Vector<T>>(mt) {}
-
-    Matrix(const Vector<Vector<T>>& vv):Vector<Vector<T>>(vv) {}
-
-    
-    Matrix& operator=(const Matrix& mt){
-        return Vector<Vector<T>>::operator=(mt);
-    }
-
-    Matrix operator+(const Matrix& mt){
-        return Vector<Vector<T>>::operator+(mt);
-    }
-
-    Matrix operator-(const Matrix& mt){
-        return Vector<Vector<T>>::operator-(mt);
-    }
-
-    void operator/(const T& elem){
-        for (int i = 0; i < this->_size; i++){
-            this->_array[i] = this->_array[i]/elem;
+    Matrix (size_t n) : Vector<Vector<T>>(n,0){
+        for (size_t i = 0; i < n; i++){
+            this->_array[i] = Vector<T> (n-i, i);
         }
     }
+    Matrix(const Matrix& mt) : Vector<Vector<T>>(mt){
 
-    void PowTo_1(){
-        _deg = -1;
-        T det;
-        Vector<T> tmp_vec;
-        Matrix<double> res(*this);
-        for(size_t i = 0;i < this->_size; i++){
-            Vector<double> tmp(this->_array[i].GetSize());
-            for (size_t j = 0;j < this->_array[i].GetSize(); j++)
-                if(i == j){ tmp[j] = 1/res._array[i].GetElem(0);}                
-                else if (i != 0 && i != this->GetSize()) {tmp[j] = res._array[i].GetElem(1)/(res._array[i-1].GetElem(0)*res._array[i].GetElem(0)) ;} 
-            this->_array[i] = tmp;
+    }                    // копирование
+    Matrix(const Vector<Vector<T> >& mt) : Vector<Vector<T>>(mt){
+
+    } // преобразование типа
+    Matrix& operator= (const Matrix& mt){
+        this->_size = mt._size;
+        this->_startIndex = mt._startIndex;
+        for (size_t i = 0; i < this->_size; i++){
+            delete [] this->_array[i];
+            this->_array[i] = nullptr;
+            this->_array[i] = mt._array[i];
         }
 
-    }
+        return *this;
+   }        // присваивание
+    Matrix  operator+ (const Matrix& mt){
+        return Vector<Vector<int>> :: operator+(mt);
+    }        // сложение
+    Matrix  operator- (const Matrix& mt){
+        return Vector<Vector<int>> :: operator-(mt);
+    }       // вычитание
 
     Matrix operator*(const Matrix& mt) {
-        if (this->_size != mt._size) {
-            throw invalid_argument("Matrices have incompatible sizes for multiplication.");
-        }
-
-        Matrix result(this->_size);
-
-        for (size_t i = 0; i < this->_size; i++) { 
-            for (size_t j = i; j < this->_size; j++) { 
-                T sum = 0;
-
-                for (size_t k = i; k <= j; k++) {
-                    if ((k - i) < this->_array[i].GetSize() && (j - k) < mt._array[k].GetSize()) {
-                        sum += this->_array[i][k - i] * mt._array[k][j - k];
-                    }
-                }
-
-                result._array[i][j - i] = sum; 
-            }
-        }
-
-        return result;
+    if (this->_size != mt._size) {
+        throw std::invalid_argument("Matrices have incompatible sizes for multiplication.");
     }
+
+    Matrix result(this->_size);
+
+    for (size_t i = 0; i < this->_size; i++) { 
+        for (size_t j = i; j < this->_size; j++) { 
+            T sum = 0;
+
+            for (size_t k = i; k <= j; k++) {
+                if ((k - i) < this->_array[i].GetSize() && (j - k) < mt._array[k].GetSize()) {
+                    sum += this->_array[i][k - i] * mt._array[k][j - k];
+                }
+            }
+
+            result._array[i][j - i] = sum; 
+        }
+    }
+
+    return result;
+}
 
     // ввод / вывод
     friend istream& operator>>(istream& in, Matrix& mt)
@@ -87,14 +69,10 @@ public:
     friend ostream& operator<<(ostream& os, const Matrix& mt)
     {
 
-
         for (int i = 0; i < mt._size; i++){
-            os << "|";
-            for (size_t j = 0; j < i; j++) os << "0 ";
-            os << mt._array[i] << "|" << endl;
-        }
-        
-
+        os << "|";
+        for (size_t j = 0; j < i; j++) os << "0, ";
+        os << mt._array[i] << "|" << endl;}
         return os;
     }
 };
